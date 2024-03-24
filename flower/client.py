4cf3031517272cca32cec9849e7d70e56f78199f
andresantosr <andresanro03@gmail.com>
2024-03-24 20:45:18 -0300

    erro no server

--- flower/client.py
+++ flower/client.py
@@ -1,29 +1,40 @@
 import flwr as fl
+from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 import pickle
 from sklearn.metrics import accuracy_score, log_loss
 import utils
+import numpy as np
+import pandas as pd
+from flwr_datasets import FederatedDataset
+import argparse
+
 
 
 with open('data/processed/loan_status.pkl','rb') as f:
     x_treino,y_treino,x_teste,y_teste = pickle.load(f)
 
-
 model = DecisionTreeClassifier()
+
 
 class TreeClient(fl.client.NumPyClient):
     def get_parameters(self, config):
         return model.get_params()
   
-    def fit(self,parameters,config):
-        #model = utils.set_model_params(model,parameters)
-        model.fit(x_treino,y_treino)
-        accuracy  = accuracy_score(y_teste,model.predict(x_teste))
-        return (utils.get_model_params(model), len(x_treino), {"accuracy": accuracy})
+    def fit(self,parameters,config):     
+        acc = []
+        #utils.set_model_params(model,parameters)
+        acc  = utils.get_accuracy(model,x_treino,y_treino,x_teste,y_teste)
+        return (utils.get_model_params(model), len(x_treino), {"accuracy": acc})
    
     def evaluate(self,parameters,config):
+        acc = []
+        loss = []
         #utils.set_model_params(model,parameters)
-        prediction = model.predict(x_teste)
+        model.fit(x_treino,y_treino)
+        prediction = model.predict(x_teste).round()
+
+
         acc  = accuracy_score(y_teste,prediction)
         loss = log_loss(y_teste, prediction)
         return loss, len(x_teste), {"accuracy": acc}
--- flower/utils.py
+++ flower/utils.py
@@ -2,6 +2,7 @@ import os
 import flwr as fl
 from sklearn.tree import DecisionTreeClassifier
 import numpy as np
+from sklearn.metrics import accuracy_score
 
 
 def get_model_params(model):
@@ -11,6 +12,29 @@ def get_model_params(model):
     return params
 
 def set_model_params(model: DecisionTreeClassifier, params) -> DecisionTreeClassifier:
-    for key, v in zip(model.get_params().keys(), params):
-        model.set_params(key=v)
+    names = ['ccp_alpha', 'class_weight', 'criterion', 'max_depth', 
+     'max_features', 'max_leaf_nodes', 'min_impurity_decrease', 
+     'min_samples_leaf', 'min_samples_split', 'min_weight_fraction_leaf', 
+     'random_state', 'splitter']
+   
+    model.set_params(ccp_alpha=params[0])
+    model.set_params(class_weight=params[1])
+    model.set_params(criterion=params[2])
+    model.set_params(max_depth=params[3])
+    model.set_params(max_features=params[4])
+    model.set_params(max_leaf_nodes=params[5])
+    model.set_params(min_impurity_decrease=params[6])
+    model.set_params(min_samples_leaf=params[7])
+    model.set_params(min_samples_split=params[8])
+    model.set_params(min_weight_fraction_leaf=params[9])
+    model.set_params(random_state=params[10])
+    model.set_params(splitter=params[11])
+
     return model
+
+def get_accuracy(model,x_treino,y_treino,x_teste,y_teste):
+    model.fit(x_treino,y_treino)
+    prediction = model.predict(x_teste).round()
+    
+    acc  = accuracy_score(y_teste,prediction)
+    return acc
